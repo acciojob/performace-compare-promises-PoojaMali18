b98629f1c061e897f2cea09a3d9b1faa5a876ba2
PoojaMali18 <141479524+PoojaMali18@users.noreply.github.com>
2023-11-29 15:52:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,5 +11,36 @@ const apiUrls = [
   "https://jsonplaceholder.typicode.com/todos/9",
   "https://jsonplaceholder.typicode.com/todos/10",
 ];
+function fetchData(url) {
+  return fetch(url).then((response) => response.json());
+}
 
-// You can write your code here
+function measureTimeTaken(promiseFunction) {
+  const startTime = performance.now();
+  return promiseFunction()
+    .then(() => {
+      const endTime = performance.now();
+      return endTime - startTime;
+    })
+    .catch((error) => {
+      console.error("Error occurred:", error);
+      return -1; // Return -1 to indicate an error
+    });
+}
+
+const outputAll = document.getElementById("output-all");
+const outputAny = document.getElementById("output-any");
+
+const promiseAllTime = measureTimeTaken(() => Promise.all(apiUrls.map((url) => fetchData(url))));
+const promiseAnyTime = measureTimeTaken(() => Promise.any(apiUrls.map((url) => fetchData(url))));
+
+Promise.all([promiseAllTime, promiseAnyTime])
+  .then((times) => {
+    outputAll.innerText = `${times[0].toFixed(2)} ms`;
+    outputAny.innerText = `${times[1].toFixed(2)} ms`;
+  })
+  .catch((error) => {
+    console.error("Error occurred:", error);
+    outputAll.innerText = "Error";
+    outputAny.innerText = "Error";
+  });
